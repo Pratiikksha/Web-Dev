4e03e54e1da50a4883fac06509d1b1dd65f3d153
Pratiikksha <psah_be23@thapar.edu>
2024-07-17 15:30:59 +0530

    Config

---
+++ Simon Game/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,70 @@
+var buttonColours = ["green", "red", "yellow", "blue"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+$(".btn").click(makeSound);
+
+function makeSound() {
+    var whichId = $(this).attr("id");
+    userClickedPattern.push(whichId);
+
+    playSound(whichId);
+    animate(whichId);
+
+    checkAnswer(userClickedPattern.length - 1);
+}
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    animate(randomChosenColour);
+    playSound(randomChosenColour);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+function animate(effect) {
+    $("#" + effect).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+
+    else {
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+        $("#level-title").text("Game-Over, Press Any Key to Restart");
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+
+
